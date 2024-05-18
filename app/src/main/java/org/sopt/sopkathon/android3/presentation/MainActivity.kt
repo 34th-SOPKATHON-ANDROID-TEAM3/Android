37b474bf058fa89a33310da221dcff7839928715
Sangwook123 <operawook@catholic.ac.kr>
2024-05-18 21:52:06 +0900

    [feat] #8 shake sensor

--- app/src/main/java/org/sopt/sopkathon/android3/presentation/MainActivity.kt
+++ app/src/main/java/org/sopt/sopkathon/android3/presentation/MainActivity.kt
@@ -1,14 +1,62 @@
 package org.sopt.sopkathon.android3.presentation
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import org.sopt.sopkathon.android3.R
+import org.sopt.sopkathon.android3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var mSensorManager: SensorManager
+    private var mAccelerometer: Sensor? = null
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        SensorUtil.init()
+        mSensorManager.registerListener(
+            sensorEventListener,
+            mAccelerometer,
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+    override fun onPause() {
+        mSensorManager.unregisterListener(sensorEventListener)
+        super.onPause()
+    }
+
+    private val sensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent?) {
+
+            if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+                val axisX = event.values[0]
+                val axisY = event.values[1]
+                val axisZ = event.values[2]
+
+                if (SensorUtil.isShake(axisX, axisY, axisZ)) {
+                    Log.e("흔들림", count++.toString())
+                }
+            }
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+        }
     }
 }
